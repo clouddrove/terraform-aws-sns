1a8f725b12fea8f28beef8b42d453c8216fbb699
Nikita Dugar <nikita@clouddrove.com>
2019-07-15 11:22:36 +0530

    update

--- _example/GCM/example.tf
+++ _example/GCM/example.tf
@@ -6,7 +6,7 @@ module "sns" {
   source = "git::https://github.com/clouddrove/terraform-aws-sns.git"
   name        = "clouddrove"
   platform    = "GCM"
-  key         = "AAAAKHQA81w:APA91bEgwftAYq6N2YV8TeU2k4bRj0k1q2I7Q47ZvFPLQm-ESbD2Fhjj3U9wNNuZ2aC-QZVdgDrN5C6E3Ec08AWhMgbs4b72gNvqcXh1JBoR6yLNtTR4yRmT0vWdhz4_PW1AwDC0aVoH"
+  key         = "AAAAKHQaqe1w:APA91bEgwftAYq6N2YV8TeU2k4bRj0k1q2I7Q47ZvFPLQm-ESbD2Fhjj3U9wNNuZ2aC-QZVdgDrN5C6E3Ec08AWhMgbs4b72gNvqcXh1JBoR6yLwretghyjutTR4yRmT0vWdhz4_PW1AwDC0aVoH"
 }
 
 
