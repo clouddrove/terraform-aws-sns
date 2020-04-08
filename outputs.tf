5d0beb17f97b2bdbf6cc246e1145da5b5450d9dc
nikitadugar <nikita@clouddrove.com>
2020-04-08 21:21:10 +0530

    add some variables in output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,13 @@ output "arn" {
   value       = join("", aws_sns_platform_application.default.*.arn)
   description = "The ARN of the SNS platform application."
 }
+
+output "topic-id" {
+  value       = join("", aws_sns_topic.default.*.id)
+  description = "The ID of the SNS topic."
+}
+
+output "topic-arn" {
+  value       = join("", aws_sns_topic.default.*.arn)
+  description = "The ARN of the SNS topic."
+}
