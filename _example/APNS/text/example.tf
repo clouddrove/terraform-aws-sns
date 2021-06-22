9b2f2a7386249554066472939eca02bdda65d9a1
yadavprakash <yadavprakash.it@gmail.com>
2021-06-22 18:35:52 +0530

    update in 0.15

---
+++ _example/APNS/text/example.tf
@@ -0,0 +1,15 @@
+module "sns" {
+  source = "./../../../"
+
+  name = "text"
+  environment = "test"
+  label_order = ["name", "environment"]
+
+  enabled = true
+  enable_sms_preference = true
+  monthly_spend_limit = "1"
+  delivery_status_success_sampling_rate = "50"
+  default_sender_id       = "test"
+  default_sms_type  = "Transactional"
+
+}
