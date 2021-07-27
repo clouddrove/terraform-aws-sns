1e6eada813ca69595b8b6dd0a227746bc234b0d8
Nikita Dugar <nikita@clouddrove.com>
2021-07-27 14:59:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -282,7 +282,7 @@ variable "subscription_delivery_policy" {
 
 variable "enable_sms_preference" {
   type        = bool
-  default     = true
+  default     = false
   description = "Boolean indicating whether or not to update SNS SMS Preference."
 }
 
