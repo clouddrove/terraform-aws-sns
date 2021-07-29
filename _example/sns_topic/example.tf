4634287b0d19e244172c4f6b8bdf213f52e7e214
anmolnagpal <ianmolnagpal@gmail.com>
2021-07-29 16:23:20 +0530

    add multiple subscription

--- _example/sns_topic/example.tf
+++ _example/sns_topic/example.tf
@@ -12,16 +12,22 @@ module "sns" {
 
   subscribers = {
     newrelic = {
-      protocol               = "https"
-      endpoint               = "https://example.com"
-      endpoint_auto_confirms = false
-      raw_message_delivery   = true
+      protocol                        = "https"
+      endpoint                        = "https://example.com"
+      endpoint_auto_confirms          = false
+      raw_message_delivery            = true
+      filter_policy                   = ""
+      delivery_policy                 = ""
+      confirmation_timeout_in_minutes = "60"
     },
     sms = {
-      protocol               = "sms"
-      endpoint               = "917742xxxxxxx"
-      endpoint_auto_confirms = false
-      raw_message_delivery   = false
+      protocol                        = "sms"
+      endpoint                        = "917742xxxxxxx"
+      endpoint_auto_confirms          = false
+      raw_message_delivery            = false
+      filter_policy                   = ""
+      delivery_policy                 = ""
+      confirmation_timeout_in_minutes = "60"
     },
 
   }
